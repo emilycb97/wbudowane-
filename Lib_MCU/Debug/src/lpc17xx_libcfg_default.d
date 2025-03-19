a98edca7f31074ae14c874601a5f3adb7c5e3ed4
unknown <student@L107-11.helium.com>
2025-03-19 15:03:45 +0100

    initial commit

---
+++ Lib_MCU/Debug/src/lpc17xx_libcfg_default.d
@@ -0,0 +1,6 @@
+src/lpc17xx_libcfg_default.o src/lpc17xx_libcfg_default.d: \
+ ../src/lpc17xx_libcfg_default.c \
+ C:\Users\student\Desktop\basic\lpc17xx_xpr_bb_140609(1)\Lib_MCU\inc/lpc17xx_libcfg_default.h \
+ C:\Users\student\Desktop\basic\lpc17xx_xpr_bb_140609(1)\Lib_MCU\inc/lpc_types.h
+C:\Users\student\Desktop\basic\lpc17xx_xpr_bb_140609(1)\Lib_MCU\inc/lpc17xx_libcfg_default.h:
+C:\Users\student\Desktop\basic\lpc17xx_xpr_bb_140609(1)\Lib_MCU\inc/lpc_types.h:
