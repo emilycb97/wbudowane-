1705b15961c8b54b41789c7ed956a3d9569983b4
unknown <student@L107-11.helium.com>
2025-03-19 17:55:20 +0100

    before leaving

--- oled_periph/src/main.c
+++ oled_periph/src/main.c
@@ -28,59 +28,6 @@
 static uint32_t msTicks = 0;
 static uint8_t buf[10];
 
-static void intToString(int value, uint8_t* pBuf, uint32_t len, uint32_t base)
-{
-    static const char* pAscii = "0123456789abcdefghijklmnopqrstuvwxyz";
-    int pos = 0;
-    int tmpValue = value;
-
-    // the buffer must not be null and at least have a length of 2 to handle one
-    // digit and null-terminator
-    if (pBuf == NULL || len < 2)
-    {
-        return;
-    }
-
-    // a valid base cannot be less than 2 or larger than 36
-    // a base value of 2 means binary representation. A value of 1 would mean only zeros
-    // a base larger than 36 can only be used if a larger alphabet were used.
-    if (base < 2 || base > 36)
-    {
-        return;
-    }
-
-    // negative value
-    if (value < 0)
-    {
-        tmpValue = -tmpValue;
-        value    = -value;
-        pBuf[pos++] = '-';
-    }
-
-    // calculate the required length of the buffer
-    do {
-        pos++;
-        tmpValue /= base;
-    } while(tmpValue > 0);
-
-
-    if (pos > len)
-    {
-        // the len parameter is invalid.
-        return;
-    }
-
-    pBuf[pos] = '\0';
-
-    do {
-        pBuf[--pos] = pAscii[value % base];
-        value /= base;
-    } while(value > 0);
-
-    return;
-
-}
-
 void SysTick_Handler(void) {
     msTicks++;
 }
